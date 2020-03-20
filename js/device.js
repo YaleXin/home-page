9bc4966426e5fc87dbeafde911ea0ce3c2b56179
“RuiXin_Huang <495592364@qq.com>
2020-03-20 22:48:57 +0800

    测试背景

--- js/device.js
+++ js/device.js
@@ -2,41 +2,21 @@
  * @Description: 
  * @Author: Yale_Xin
  * @Date: 2020-03-20 18:57:13
- * @LastEditTime: 2020-03-20 21:18:06
+ * @LastEditTime: 2020-03-20 22:30:03
  * @LastEditors: Yale_Xin
  */
 //判断设备类型
 try {
     if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
         console.log("移动设备");
-        if (document.getElementById("app").className == "") {
-            document.getElementById("app").className = "phone";
-        } else {
-            // 如果原来有class
-            var arrClassName = document.getElementById("app").className.split("");
-            var _index = arrIndexOf(arrClassName, "phone");
-            if (_index == -1) {
-                // 如果要添加的class在原来的class中不存在
-                document.getElementById("app").className += "" + "phone";
-            }
-            //  如果要添加的class在原来的class中存在
-        }
-        //div的宽度
         console.log(document.getElementById("app").offsetWidth);
+        // document.getElementById("app").style.height="100px";
+        console.log(document.getElementById("app").offsetHeight);
+        
         
     } else {
         console.log("pc设备");
-        if (document.getElementById("app").className == "") {
-            document.getElementById("app").className = "pc";
-        } else {
-            // 如果原来有class
-            var arrClassName = document.getElementById("app").className.split("");
-            var _index = arrIndexOf(arrClassName, "pc");
-            if (_index == -1) {
-                // 如果要添加的class在原来的class中不存在
-                document.getElementById("app").className += "" + "pc";
-            }
-            //  如果要添加的class在原来的class中存在
-        }
+        document.getElementById("app").className = "pc";
+        
     }
 } catch (e) { }
