866cedd74aa8f18200801dfaf87ae5e6f4b61524
“RuiXin_Huang <495592364@qq.com>
2020-03-21 14:54:29 +0800

    测试动画

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Yale_Xin
  * @Date: 2020-03-20 13:14:06
- * @LastEditTime: 2020-03-21 12:58:49
+ * @LastEditTime: 2020-03-21 14:53:29
  * @LastEditors: Yale_Xin
  */
 // vue的逻辑代码
@@ -19,12 +19,12 @@ var app = new Vue({
             "https://www.so.com/s?q=",
         ],
         s_engine: ["必应", "百度", "谷歌", "搜狗", "360"],
-        engine_url:"",
+        engine_url: "",
     },
     methods: {
         //按下回车键
         enter: function () {
-            if(this.engine_url == ""){
+            if (this.engine_url == "") {
                 this.engine_url = this.list_engine[0];
             }
             window.location.href = this.engine_url + this.input;
@@ -38,11 +38,12 @@ var app = new Vue({
         // 失去焦点
         blur: function () {
             console.log("失去");
-
+            document.getElementById("input").style.animationName = "input_width2short";
+            document.getElementById("input").style.animationDuration = "500ms";
         },
         change: function () {
             var sl_index = document.getElementById("plan").selectedIndex;
-            console.log("change:"+sl_index);
+            console.log("change:" + sl_index);
             this.engine_url = this.list_engine[sl_index];
         },
     }
