629bae9c89112dec5e2a5ddfea51fe5f8e730b4b
“RuiXin_Huang <495592364@qq.com>
2020-03-21 11:39:06 +0800

    有搜索引擎

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Yale_Xin
  * @Date: 2020-03-20 13:14:06
- * @LastEditTime: 2020-03-20 20:15:09
+ * @LastEditTime: 2020-03-21 11:38:50
  * @LastEditors: Yale_Xin
  */
 // vue的逻辑代码
@@ -10,7 +10,9 @@ var app = new Vue({
     el: "#app",
     data: {
         input: "",
-        isPhone:false,
+        isPhone: false,
+        select: false,
+        s_engine: ["必应", "百度", "谷歌", "搜狗", "雅虎", "360", "SOSO", "有道"],
     },
     methods: {
         //按下回车键
@@ -27,23 +29,26 @@ var app = new Vue({
         blur: function () {
             console.log("失去");
 
-        }
+        },
+        change: function () {
+            console.log("change");
+        },
     }
 });
 
 
 //时钟更新
-function setTime(){
-    setInterval("uploadTime()",1000);
+function setTime() {
+    setInterval("uploadTime()", 1000);
 }
-function uploadTime(){
+function uploadTime() {
     var time = new Date();
     var h = time.getHours();
     var m = time.getMinutes();
     var s = time.getSeconds();
-    h = h<10?("0"+h):h;
-    m = m<10?("0"+m):m;
-    s = s<10?("0"+s):s;
-    document.getElementById("clock").innerHTML = h+":"+m+":"+s;
+    h = h < 10 ? ("0" + h) : h;
+    m = m < 10 ? ("0" + m) : m;
+    s = s < 10 ? ("0" + s) : s;
+    document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
 }
 
