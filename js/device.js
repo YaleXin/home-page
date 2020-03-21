e4fd28ff908af7db8e7b4089493f4fac7246cb84
“RuiXin_Huang <495592364@qq.com>
2020-03-21 09:28:36 +0800

    测试固定

--- js/device.js
+++ js/device.js
@@ -2,19 +2,19 @@
  * @Description: 
  * @Author: Yale_Xin
  * @Date: 2020-03-20 18:57:13
- * @LastEditTime: 2020-03-20 23:39:44
+ * @LastEditTime: 2020-03-21 09:25:03
  * @LastEditors: Yale_Xin
  */
 //判断设备类型
 try {
     if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
         console.log("移动设备");
-        console.log(document.getElementById("app").offsetWidth);
+        // console.log(document.getElementById("app").offsetWidth);
         // document.getElementById("app").style.height="100px";
-        console.log(document.getElementById("app").offsetHeight);
-        console.log("width:"+document.getElementById("input_div").offsetWidth);
+        // console.log(document.getElementById("app").offsetHeight);
+        // console.log("width:"+document.getElementById("input_div").offsetWidth);
         
-        document.getElementById("input_div").style.height = document.getElementById("input_div").offsetWidth*0.08.toString()+"px";
+        // document.getElementById("input_div").style.height = document.getElementById("input_div").offsetWidth*0.08.toString()+"px";
         
     } else {
         console.log("pc设备");
